c700d39586dc8237d1b7d1c81de384f1475d8df8
Hawra <hawraaar@stud.ntnu.no>
2021-04-26 12:08:52 +0200

    adding client id

--- Components/WalkieCommander.py
+++ Components/WalkieCommander.py
@@ -17,49 +17,21 @@ MQTT_BROKER = 'mqtt.item.ntnu.no'
 MQTT_PORT = 1883
 
 # TODO: choose proper topics for communication
-MQTT_TOPIC_INPUT = 'ttm4115/team_07/Channel2'
-MQTT_TOPIC_OUTPUT = 'ttm4115/team_07/Channel2'
+MQTT_TOPIC_INPUT = 'ttm4115/team_07/Channel'
+MQTT_TOPIC_OUTPUT = 'ttm4115/team_07/Channel'
 
 
 
 class WalkieTalkie:
 
     """
-    The component to send voice commands.
+    The component to send and receive voice messages.
     """
-    def on_connect(self, client, userdata, flags, rc):
-        print("hallo")
-        self._logger.debug('MQTT connected to {}'.format(client))
-
-    def on_message(self, client, userdata, msg):
-        print("A message is received")
-        self._logger.debug('Incoming message to topic {}'.format(msg.topic))
-        # encdoding from bytes to string. This
-        temp_file = str(strftime("%Y-%m-%d %H-%M-%S", gmtime())) + ".wav"
-        self.temp_file = temp_file
-        f = open("../player/" + temp_file, 'wb')
-        f.write(msg.payload)
-        print("bbbbbb")
-        f.close()
-        self.driver.send('start', 'stm_player', args=[self.temp_file])
-
-    def send_message(self):
-        print("hei")
-        path = self.recorder.get_latest_file()
-        f = open(path, "rb")
-        imagestring = f.read()
-        f.close()
-        byteArray = bytearray(imagestring)
-        print("hei2")
-
-        #self.mqtt_client.publish(MQTT_TOPIC_OUTPUT, "TEST")
-
-        publish.single(MQTT_TOPIC_OUTPUT, byteArray, hostname=MQTT_BROKER, port=MQTT_PORT)
-        time.sleep(1)
-        print("hei3")
-
 
     def __init__(self):
+        # setting the standard channel as 0
+        self.channel = 0
+        self.ID = "default"
         # get the logger object for the component
         self._logger = logging.getLogger(__name__)
         print('logging under name {}.'.format(__name__))
@@ -74,10 +46,11 @@ class WalkieTalkie:
         # Connect to the broker
         self.mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
         # subscribe to proper topic(s) of your choice
-        self.mqtt_client.subscribe(MQTT_TOPIC_INPUT)
+
+        self.mqtt_client.subscribe(MQTT_TOPIC_INPUT + str(self.channel))
+
         # start the internal loop to process MQTT messages
         self.mqtt_client.loop_start()
-
 
         recorder = Recorder.create_machine('stm_recorder', self)
         self.recorder = recorder
@@ -91,43 +64,101 @@ class WalkieTalkie:
         self.driver.add_machine(stm_recorder)
         self.driver.add_machine(stm_player)
 
-
         self.driver.start(keep_active = True)
         self.create_gui()
         print("Help!")
 
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("hallo")
+        self._logger.debug('MQTT connected to {}'.format(client))
+        self.client_id = client
+
+    def on_message(self, client, userdata, msg):
+        print("A message is received")
+        self._logger.debug('Incoming message to topic {}'.format(msg.topic))
+        # encdoding from bytes to string. This
+        if (self.client_id != client):
+            temp_file = str(strftime("%Y-%m-%d %H-%M-%S", gmtime())) + ".wav"
+            self.temp_file = temp_file
+            f = open("../player/" + temp_file, 'wb')
+            f.write(msg.payload)
+            print("bbbbbb")
+            f.close()
+            self.driver.send('start', 'stm_player', args=[self.temp_file])
+
+    def send_message(self):
+        print("hei")
+        path = self.recorder.get_latest_file()
+        f = open(path, "rb")
+        imagestring = f.read()
+        f.close()
+        byteArray = bytearray(imagestring)
+        print("hei2")
+
+        #self.mqtt_client.publish(MQTT_TOPIC_OUTPUT, "TEST")
+
+        publish.single(MQTT_TOPIC_OUTPUT+str(self.channel), byteArray, client_id=self.ID, hostname=MQTT_BROKER, port=MQTT_PORT)
+        time.sleep(1)
+        print("hei3")
+
     def create_gui(self):
         self.app = gui()
 
+        # Choose ID
+        self.app.addLabel("NameLabel", "User: " + self.ID)
+        self.app.addLabelEntry("NameEntry")
+        def on_button_name(title):
+            self.ID = self.app.getEntry("NameEntry")
+            self.app.setLabel("NameLabel", "User: " + self.ID)
+            print("ASDFDASFSA")
+        self.app.setEntrySubmitFunction("NameEntry", on_button_name)
 
-        def extract_timer_name(label):
-            label = label.lower()
-            if 'start' in label: return 'start'
-            if 'stop'  in label: return 'stop'
-            return None
 
-        self.app.startLabelFrame('Start recording:')
+        # Choose channel
+        self.app.startLabelFrame('Change channel:')
+        self.app.addLabel("Channel",text="Connected to channel: {}".format(self.channel))
+        def on_button_pressed_increase(title):
+            self.mqtt_client.unsubscribe(MQTT_TOPIC_INPUT + str(self.channel))
+            self.channel +=1
+            self.mqtt_client.subscribe(MQTT_TOPIC_INPUT + str(self.channel))
+            self.app.setLabel("Channel",text="Connected to channel: {}".format(self.channel))
+
+        self.app.addButton('Increase', on_button_pressed_increase)
+
+        def on_button_pressed_decrease(title):
+            self.mqtt_client.unsubscribe(MQTT_TOPIC_INPUT + str(self.channel))
+            self.channel -= 1
+            self.mqtt_client.subscribe(MQTT_TOPIC_INPUT + str(self.channel))
+            self.app.setLabel("Channel",text="Connected to channel: {}".format(self.channel))
+
+        self.app.addButton('Decrease', on_button_pressed_decrease)
+        self.app.stopLabelFrame()
+
+
+        # Start and stop recording
+        self.app.startLabelFrame('Recordings:')
         print("1")
 
         def on_button_pressed_start(title):
             self.driver.send('start', 'stm_recorder')
             print("2")
 
-        self.app.addButton('Start', on_button_pressed_start)
+        self.app.addButton('Start recording', on_button_pressed_start)
         print("3")
-        self.app.stopLabelFrame()
         print("4")
 
-        self.app.startLabelFrame('Stop recording:')
         def on_button_pressed_stop(title):
             self.driver.send('stop', 'stm_recorder')
             time.sleep(2)
             self.send_message()
         print('5')
-        self.app.addButton('Stop recording', on_button_pressed_stop)
+        self.app.addButton('Stop and send recording', on_button_pressed_stop)
         self.app.stopLabelFrame()
 
-        self.app.startLabelFrame('Replay:')
+
+        # Replay last received message
+        self.app.startLabelFrame('Replay last message:')
         def on_button_pressed_play(title):
             self.driver.send('start', 'stm_player', args=[self.temp_file])
         self.app.addButton('Replay', on_button_pressed_play)
